c2db07b148707394a9a1070d00983a9533caf07c
CodeFather <36696181+brhngksel26@users.noreply.github.com>
2021-06-18 21:28:23 +0300

    Update settings.py

--- meet/settings.py
+++ meet/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'brainsforce@gmail.com'
-EMAIL_HOST_PASSWORD = 'CodeFather26!26CodeFather'
+EMAIL_HOST_PASSWORD = 'asd'
