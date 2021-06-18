547551df2686dbe11d55ee0d0b686f0d0774afab
CodeFather <36696181+brhngksel26@users.noreply.github.com>
2021-06-18 18:46:41 +0300

    Update settings.py

--- meet/settings.py
+++ meet/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -130,6 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+django_heroku.settings(locals())
 
 MEDIA_URL = '/images/'
 
